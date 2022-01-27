b0f9f025496004bc84b23453a7946f2156aadaf3
Chetonix <chetonix@gmail.com>
2022-01-27 16:22:21 +0530

    add some more faulty code to the game

--- game.js
+++ game.js
@@ -1,7 +1,9 @@
 var player1Health = 100;
 var player2Health = 100;
 var gameStarted = false;
-var wonMatches = [0, 0];
+var wonMatchesPlayer1 = 0;
+var wonMatchesPlayer2 = 0;
+
 document.getElementById("btn-start").addEventListener("click", startGame);
 
 var startGame = function () {
@@ -15,10 +17,9 @@ document.getElementById("shoot-1").addEventListener("click", function () {
     "Health: " + player1Health;
 
   if (player1Health <= 0) {
-    wonMatches[0] += 0;
-    wonMatches[1] += 1;
-    document.getElementById("player-1-heading").textContent =
-      "Player 1 - Won: " + wonMatches[0];
+    wonMatchesPlayer2 += 1;
+    document.getElementById("player-2-heading").textContent =
+      "Player 2 - Won: " + wonMatchesPlayer2;
   }
 });
 
@@ -29,9 +30,8 @@ document.getElementById("shoot-2").addEventListener("click", function () {
     "Health: " + player2Health;
 
   if (player2Health <= 0) {
-    wonMatches[0] += 1;
-    wonMatches[1] += 0;
-    document.getElementById("player-2-heading").textContent =
-      "Player 1 - Won: " + wonMatches[1];
+    wonMatchesPlayer1 += 1;
+    document.getElementById("player-1-heading").textContent =
+      "Player 1 - Won: " + wonMatchesPlayer1;
   }
 });
