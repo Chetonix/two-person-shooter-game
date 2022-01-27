aff08905b737095dee8dc338eb63919cebcb33e8
Chetonix <chetonix@gmail.com>
2022-01-27 16:13:32 +0530

    add some more code to the game.js file.

--- game.js
+++ game.js
@@ -1,26 +1,37 @@
+var player1Health = 100;
+var player2Health = 100;
+var gameStarted = false;
+var wonMatches = [0, 0];
 document.getElementById("btn-start").addEventListener("click", startGame);
 
 var startGame = function () {
   gameStarted = true;
-
-  while (gameStarted) {
-    var player1Health = 100;
-    var player2Health = 100;
-
-    document.getElementById("shoot-1").addEventListener("click", function () {
-      console.log(player1Health);
-      player1Health -= Math.floor(Math.random() * 5) + 1;
-
-      document.getElementById("player-1-health").textContent =
-        "Health: " + player1Health;
-    });
-
-    document.getElementById("shoot-2").addEventListener("click", function () {
-      console.log(player2Health);
-      player2Health -= Math.floor(Math.random() * 5) + 1;
-
-      document.getElementById("player-2-health").innerHTML =
-        "Health: " + player2Health;
-    });
-  }
 };
+
+document.getElementById("shoot-1").addEventListener("click", function () {
+  player1Health -= Math.floor(Math.random() * 5) + 1;
+
+  document.getElementById("player-1-health").textContent =
+    "Health: " + player1Health;
+
+  if (player1Health <= 0) {
+    wonMatches[0] += 0;
+    wonMatches[1] += 1;
+    document.getElementById("player-1-heading").textContent =
+      "Player 1 - Won: " + wonMatches[0];
+  }
+});
+
+document.getElementById("shoot-2").addEventListener("click", function () {
+  player2Health -= Math.floor(Math.random() * 5) + 1;
+
+  document.getElementById("player-2-health").innerHTML =
+    "Health: " + player2Health;
+
+  if (player2Health <= 0) {
+    wonMatches[0] += 1;
+    wonMatches[1] += 0;
+    document.getElementById("player-2-heading").textContent =
+      "Player 1 - Won: " + wonMatches[1];
+  }
+});
