0bbf4d177134d0db7ea132ad011dcc7038d9d6bd
Chetonix <chetonix@gmail.com>
2022-01-28 15:10:44 +0530

    add more than half of the code to the repo

--- game.js
+++ game.js
@@ -12,10 +12,14 @@ var startGame = function () {
 };
 
 document.getElementById("shoot-1").addEventListener("click", function () {
-  player1Health -= Math.floor(Math.random() * 5) + 1;
+  if (activePlayer1) {
+    player1Health -= Math.floor(Math.random() * 5) + 1;
 
-  document.getElementById("player-1-health").textContent =
-    "Health: " + player1Health;
+    document.getElementById("player-1-health").textContent =
+      "Health: " + player1Health;
+    activePlayer1 = false;
+    activePlayer2 = true;
+  }
 
   if (player1Health <= 0) {
     wonMatchesPlayer1 += 0;
@@ -31,13 +35,31 @@ document.getElementById("shoot-1").addEventListener("click", function () {
     document.getElementById("player-2-health").innerHTML =
       "Health: " + player2Health;
   }
+  if (wonMatchesPlayer1 === 3 || wonMatchesPlayer2 === 3) {
+    if (wonMatchesPlayer1 === 3) {
+      document.getElementById("player-1-heading").textContent =
+        "Player 1 Wins the tournament!";
+      activePlayer1 = false;
+      activePlayer2 = false;
+    }
+    if (wonMatchesPlayer2 === 3) {
+      document.getElementById("player-2-heading").textContent =
+        "Player 2 Wins the tournament!";
+      activePlayer1 = false;
+      activePlayer2 = false;
+    }
+  }
 });
 
 document.getElementById("shoot-2").addEventListener("click", function () {
-  player2Health -= Math.floor(Math.random() * 5) + 1;
+  if (activePlayer2) {
+    player2Health -= Math.floor(Math.random() * 5) + 1;
 
-  document.getElementById("player-2-health").innerHTML =
-    "Health: " + player2Health;
+    document.getElementById("player-2-health").innerHTML =
+      "Health: " + player2Health;
+    activePlayer2 = false;
+    activePlayer1 = true;
+  }
 
   if (player2Health <= 0) {
     wonMatchesPlayer1 += 1;
@@ -53,17 +75,39 @@ document.getElementById("shoot-2").addEventListener("click", function () {
     document.getElementById("player-2-health").innerHTML =
       "Health: " + player2Health;
   }
+
+  if (wonMatchesPlayer1 === 3 || wonMatchesPlayer2 === 3) {
+    if (wonMatchesPlayer1 === 3) {
+      document.getElementById("player-1-heading").textContent =
+        "Player 1 Wins the tournament!";
+    }
+    if (wonMatchesPlayer2 === 3) {
+      document.getElementById("player-2-heading").textContent =
+        "Player 2 Wins the tournament!";
+    }
+  }
 });
 
-var resetGame = function () {
-  wonMatchesPlayer2 += 1;
-  document.getElementById("player-2-heading").textContent =
-    "Player 2 - Won: " + wonMatchesPlayer2;
+// var resetGame = function () {
+//   wonMatchesPlayer2 += 1;
+//   document.getElementById("player-2-heading").textContent =
+//     "Player 2 - Won: " + wonMatchesPlayer2;
 
-  wonMatchesPlayer1 += 1;
-  document.getElementById("player-1-heading").textContent =
-    "Player 1 - Won: " + wonMatchesPlayer1;
+//   wonMatchesPlayer1 += 1;
+//   document.getElementById("player-1-heading").textContent =
+//     "Player 1 - Won: " + wonMatchesPlayer1;
 
-  player1Health = 100;
-  player2Health = 100;
-};
+//   player1Health = 100;
+//   player2Health = 100;
+// };
+
+// if (wonMatchesPlayer1 === 3 || wonMatchesPlayer2 === 3) {
+//   if (wonMatchesPlayer1 === 3) {
+//     document.getElementById("player-1-heading").textContent =
+//       "Player 1 Wins the tournament!";
+//   }
+//   if (wonMatchesPlayer2 === 3) {
+//     document.getElementById("player-2-heading").textContent =
+//       "Player 2 Wins the tournament!";
+//   }
+// }
