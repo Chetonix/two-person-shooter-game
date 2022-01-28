bde38cb0ba147b9d1063cbf10fa62b91eff81c4f
Chetonix <chetonix@gmail.com>
2022-01-28 14:37:51 +0530

    put up half of the code, half done that is.

--- game.js
+++ game.js
@@ -3,7 +3,8 @@ var player2Health = 100;
 var gameStarted = false;
 var wonMatchesPlayer1 = 0;
 var wonMatchesPlayer2 = 0;
-
+var activePlayer1 = true;
+var activePlayer2 = false;
 document.getElementById("btn-start").addEventListener("click", startGame);
 
 var startGame = function () {
@@ -17,7 +18,18 @@ document.getElementById("shoot-1").addEventListener("click", function () {
     "Health: " + player1Health;
 
   if (player1Health <= 0) {
-    resetGame();
+    wonMatchesPlayer1 += 0;
+    wonMatchesPlayer2 += 1;
+    document.getElementById("player-1-heading").textContent =
+      "Player 1 - Won: " + wonMatchesPlayer1;
+    document.getElementById("player-2-heading").textContent =
+      "Player 2 - Won: " + wonMatchesPlayer2;
+    player1Health = 100;
+    player2Health = 100;
+    document.getElementById("player-1-health").textContent =
+      "Health: " + player1Health;
+    document.getElementById("player-2-health").innerHTML =
+      "Health: " + player2Health;
   }
 });
 
@@ -28,7 +40,18 @@ document.getElementById("shoot-2").addEventListener("click", function () {
     "Health: " + player2Health;
 
   if (player2Health <= 0) {
-    resetGame();
+    wonMatchesPlayer1 += 1;
+    wonMatchesPlayer2 += 0;
+    document.getElementById("player-1-heading").textContent =
+      "Player 1 - Won: " + wonMatchesPlayer1;
+    document.getElementById("player-2-heading").textContent =
+      "Player 2 - Won: " + wonMatchesPlayer2;
+    player1Health = 100;
+    player2Health = 100;
+    document.getElementById("player-1-health").textContent =
+      "Health: " + player1Health;
+    document.getElementById("player-2-health").innerHTML =
+      "Health: " + player2Health;
   }
 });
 
